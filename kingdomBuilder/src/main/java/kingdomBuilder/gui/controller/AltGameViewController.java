0b757030122cbecd32b549739b45f86e28a35e5a
Tom Weggel <bt712399@myubt.de>
2021-11-29 10:38:01 +0100

    Added Subscene to GameView

--- kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/AltGameViewController.java
+++ kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/AltGameViewController.java
@@ -2,7 +2,12 @@ package kingdomBuilder.gui.controller;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.SubScene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class AltGameViewController {
 
@@ -12,15 +17,28 @@ public class AltGameViewController {
     private Label player1;
 
     @FXML
+    private SubScene subSceneBoard;
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
     public void onButtonMainMenuPressed(Event event) {
         mainViewController.showMenuView();
+    }
+
+    @FXML
+    public void initialize() {
+        //set height and width of the subscene to the borderpanes center
+        subSceneBoard.heightProperty().set(borderPane.getCenter().getBoundsInParent().getHeight());
+        subSceneBoard.widthProperty().set(borderPane.getCenter().getBoundsInParent().getWidth());
     }
 
     public void setMainViewController(AltMainViewController mainViewController) {
         this.mainViewController = mainViewController;
     }
 
-    // Möglicherweise wegen REDUX überflüssig, dass Daten zwischen den Controllern hin und her geschoben werden
+    // TODO: Möglicherweise wegen REDUX überflüssig, dass Daten zwischen den Controllern hin und her geschoben werden
     public void setPlayer1Name(String name) {
         player1.setText(name);
     }
