663dce1e81f6b0aaa1695daa557cd93f81eac48a
Tom Weggel <bt712399@myubt.de>
2021-12-03 16:11:38 +0100

    text field now has Enter functionalities

--- kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/GameLobbyViewController.java
+++ kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/GameLobbyViewController.java
@@ -1,24 +1,50 @@
 package kingdomBuilder.gui.controller;
 
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
-public class GameLobbyViewController extends Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameLobbyViewController extends Controller implements Initializable {
 
     private MainViewController mainViewController;
 
     @FXML
     private TextField gameLobbyTextField;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setupEventHandler();
+    }
+
     @FXML
     public void onButtonGameStartPressed(Event event) {
         String playerName = gameLobbyTextField.getText();
-        if (playerName.isEmpty()) {
-
-        } else {
+        if (!playerName.isEmpty()) {
             mainViewController.showGameView(playerName);
         }
+    }
+
+    private void setupEventHandler() {
+        setupKeyEventHandler();
+    }
+
+    private void setupKeyEventHandler() {
+        gameLobbyTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                String playerName = gameLobbyTextField.getText();
+                if (!playerName.isEmpty() && event.getCode() == KeyCode.ENTER) {
+                    mainViewController.showGameView(playerName);
+                }
+            }
+        });
     }
 
     @FXML
