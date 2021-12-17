19314db55cefe1fd4d4f718192d67ecc5b475515
Tom Weggel <bt712399@myubt.de>
2021-12-17 16:54:35 +0100

    Added JavaDocs in IAmViewController.java

--- kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/IAmViewController.java
+++ kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/IAmViewController.java
@@ -14,28 +14,58 @@ import kingdomBuilder.redux.Store;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * This class controls the functions of the IAmView.
+ */
 public class IAmViewController extends Controller implements Initializable {
+    /**
+     * Represents the MainViewController for access to switch Views-methods.
+     */
     private MainViewController mainViewController;
+    /**
+     * Represents the store of the application.
+     */
     private final Store<KBState> store;
 
+    /**
+     * Represents the TextField to enter the preferred name.
+     */
     @FXML
     private TextField iAmViewTextField;
 
+    /**
+     * Constructor that sets the Store
+     * @param store
+     */
     public IAmViewController(Store<KBState> store) {
         this.store = store;
     }
 
+    /**
+     * Called to initialize this controller after its root element has been completely processed.
+     * @param location The location used to resolve relative paths for the root object,
+     *                 or null if the location is not known.
+     * @param resources The resources used to localize the root object, or null if the root object was not localized.
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setupEventHandler();
     }
 
+    /**
+     * Sets the MainViewController.
+     * @param mainViewController MainViewController with all functions.
+     */
     public void setMainViewController(MainViewController mainViewController) {
         this.mainViewController = mainViewController;
     }
 
+    /**
+     * Sets the functionality for the MainMenu Button.
+     * @param event Contains the data from the event source.
+     */
     @FXML
-    public void onButtonMainMenueShow(Event event) {
+    public void onButtonMainMenuShow(Event event) {
         if(iAmViewTextField.getText().isEmpty())
             return;
 
@@ -45,12 +75,22 @@ public class IAmViewController extends Controller implements Initializable {
         mainViewController.showMenuView();
     }
 
+    /**
+     * Setup all connected EventHandler
+     */
     private void setupEventHandler() {
         setupKeyEventHandler();
     }
 
+    /**
+     * Creates the EventHandler that is responsible for the Key events
+     */
     private void setupKeyEventHandler() {
         iAmViewTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            /**
+             * Invoked when a specific event of the type for which this handler is registered happens.
+             * @param event the event which occurred
+             */
             @Override
             public void handle(KeyEvent event) {
                 String playerName = iAmViewTextField.getText();
