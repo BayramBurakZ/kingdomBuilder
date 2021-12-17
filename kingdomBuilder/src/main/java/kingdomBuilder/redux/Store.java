1f6dc6ca5dd7d12166477ffe37681ffc9d46f5f4
Linda Lang <lindalang21@gmx.de>
2021-12-17 15:10:34 +0100

    Added JavaDocs to Store

--- kingdomBuilder/src/main/java/kingdomBuilder/redux/Store.java
+++ kingdomBuilder/src/main/java/kingdomBuilder/redux/Store.java
@@ -3,21 +3,47 @@ package kingdomBuilder.redux;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents the store of the application.
+ * @param <State> State of application.
+ */
 public class Store<State> {
+    /**
+     * Represents the state of the application.
+     */
     private State state;
+    /**
+     * Represents the reducer of the application.
+     */
     private final Reducer<State> reducer;
+    /**
+     * Represents the list of subscribers which should be notified if the state is modified.
+     */
     private final List<Subscriber<State>> subscribers;
 
+    /**
+     * Creates a new Store object with the given state and reducer.
+     * @param state State of the application.
+     * @param reducer Reducer of the application.
+     */
     public Store(State state, Reducer<State> reducer) {
         this.state = state;
         this.reducer = reducer;
         this.subscribers = new ArrayList<>();
     }
 
+    /**
+     * Gets the state of the application.
+     * @return the state of application.
+     */
     public State getState() {
         return this.state;
     }
 
+    /**
+     * Calls the reducer to get the new state and notifies all subscribers.
+     * @param action changes the state.
+     */
     public synchronized void dispatch(Action action) {
         State newState = reducer.reduce(this, this.state, action);
 
@@ -27,6 +53,10 @@ public class Store<State> {
         }
     }
 
+    /**
+     * Adds a new subscriber to the {@link #subscribers}.
+     * @param subscriber subscriber of the state that should be notified if the state changes.
+     */
     public void subscribe(Subscriber<State> subscriber) {
         subscribers.add(subscriber);
         subscriber.onChange(this.state);
