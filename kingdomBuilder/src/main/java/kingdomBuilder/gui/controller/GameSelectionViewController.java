9414dac6b3c2c3902bc15215b309da7d73dbade9
Tom Weggel <bt712399@myubt.de>
2021-12-17 16:34:49 +0100

    Added JavaDocs in GameSelectionViewController.java

--- kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/GameSelectionViewController.java
+++ kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/GameSelectionViewController.java
@@ -3,15 +3,28 @@ package kingdomBuilder.gui.controller;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 
+/**
+ * This class controls all functions for the GameSelectionView.
+ */
 public class GameSelectionViewController extends Controller {
-
+    /**
+     * Represents the MainViewController for access to switch Views-methods.
+     */
     private MainViewController mainViewController;
 
+    /**
+     * Sets the functionality for the CreateNewGame Button.
+     * @param event Contains the data from the event source.
+     */
     @FXML
     public void onButtonCreateNewGamePressed(Event event) {
         mainViewController.showGameLobbyView();
     }
 
+    /**
+     * Sets the MainViewController.
+     * @param mainViewController MainViewController with all functions.
+     */
     public void setMainViewController(MainViewController mainViewController) {
         this.mainViewController = mainViewController;
     }
