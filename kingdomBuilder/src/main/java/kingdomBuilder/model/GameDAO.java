ed6ee8b4fd23dbc3b664e806b2b1803f46fc8443
Linda Lang <lindalang21@gmx.de>
2021-12-18 16:47:56 +0100

    Added JavaDocs to GameDAO.

--- kingdomBuilder/src/main/java/kingdomBuilder/model/GameDAO.java
+++ kingdomBuilder/src/main/java/kingdomBuilder/model/GameDAO.java
@@ -2,27 +2,50 @@ package kingdomBuilder.model;
 
 import java.util.Objects;
 
+/**
+ * Stores information about the game.
+ */
 public class GameDAO {
-
+    /**
+     * Represents the id of the game.
+     */
     private int id;
+    /**
+     * Represents the name of the game.
+     */
     private String name;
 
-    public GameDAO(
-            int id,
-            String name
-    ) {
+    /**
+     * Creates game object.
+     * @param id id of the game.
+     * @param name name of the game.
+     */
+    public GameDAO(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * Gets the id of the game.
+     * @return id of the game.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets name of the game.
+     * @return name of the game.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Computes whether the game object and the given object are equal.
+     * @param obj object which is to be compared.
+     * @return whether the object is equal to this game object.
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == this) return true;
@@ -32,11 +55,19 @@ public class GameDAO {
                 Objects.equals(this.name, that.name);
     }
 
+    /**
+     * Generates hashcode for id and name of the game.
+     * @return the hashcode.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(id, name);
     }
 
+    /**
+     * Generates a string of the game and its attributes.
+     * @return string of the game and its attributes.
+     */
     @Override
     public String toString() {
         return "GameDAO[" +
