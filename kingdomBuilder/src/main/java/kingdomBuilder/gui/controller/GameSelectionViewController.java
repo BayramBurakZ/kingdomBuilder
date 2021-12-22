ef8ba12423f828dd83faf9728d5f22eac5d373c7
Sebastian Lauf <sebastian.lauf@t-online.de>
2021-12-22 16:04:32 +0100

    refactored gameselectionviewcontroller
    Co-authored-by: Tom Weggel <bt712399@myubt.de>

--- kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/GameSelectionViewController.java
+++ kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/GameSelectionViewController.java
@@ -2,15 +2,36 @@ package kingdomBuilder.gui.controller;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * This class controls all functions for the GameSelectionView.
  */
-public class GameSelectionViewController extends Controller {
+public class GameSelectionViewController extends Controller implements Initializable {
     /**
      * Represents the MainViewController for access to switch Views-methods.
      */
     private MainViewController mainViewController;
+
+    @FXML
+    private HBox hbox_gameselection;
+
+    @FXML
+    private VBox vbox_table;
+
+    @FXML
+    private VBox vbox_preview;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        vbox_table.prefWidthProperty().bind(hbox_gameselection.widthProperty().multiply(0.5));
+        vbox_preview.prefWidthProperty().bind(hbox_gameselection.widthProperty().multiply(0.5));
+    }
 
     /**
      * Sets the functionality for the CreateNewGame Button.
