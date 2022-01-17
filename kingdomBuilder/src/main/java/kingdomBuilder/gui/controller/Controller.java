f3a0436770a8a2d68e66724729b1bc5f60dfd3db
Linda Lang <lindalang21@gmx.de>
2022-01-17 22:52:20 +0100

    Modified Controller.

--- kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/Controller.java
+++ kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/Controller.java
@@ -1,6 +1,13 @@
 package kingdomBuilder.gui.controller;
 
+import java.lang.reflect.Field;
+
 /**
  * Basic class that every controller inherit so every ViewController is able to cast into this.
  */
-public class Controller { }
+public class Controller {
+    Field[] fields;
+    public Controller() {
+        fields = getClass().getFields();
+    }
+}
