07f265d11211a37e98bfd0272cfa6e0668eba5e2
Linda Lang <lindalang21@gmx.de>
2022-01-17 23:01:09 +0100

    Modified Controller.

--- kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/Controller.java
+++ kingdomBuilder/src/main/java/kingdomBuilder/gui/controller/Controller.java
@@ -1,13 +1,24 @@
 package kingdomBuilder.gui.controller;
 
+import kingdomBuilder.KBState;
+import kingdomBuilder.redux.Store;
+import kingdomBuilder.redux.Subscriber;
+
 import java.lang.reflect.Field;
 
 /**
  * Basic class that every controller inherit so every ViewController is able to cast into this.
  */
 public class Controller {
+    /**
+     * Represents the store of the application.
+     */
+    private Store<KBState> store;
     Field[] fields;
     public Controller() {
         fields = getClass().getFields();
+        for(var field: fields){
+            store.subscribe((Subscriber<KBState>) this, field.getName());
+        }
     }
 }
